ef383924f5324f46df8bdd682c85a7dd1daf1e9a
Sudip Samanta <168456650+sudipsamanta21@users.noreply.github.com>
2026-09-19 11:22:02 +0530

    Add import for Value annotation in CorsConfig

--- src/main/java/com/sudip/Hospital_management_backend/config/CorsConfig.java
+++ src/main/java/com/sudip/Hospital_management_backend/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.sudip.Hospital_management_backend.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
